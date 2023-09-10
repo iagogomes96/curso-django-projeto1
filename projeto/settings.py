b989ceb11f026797ed17557978b23c4b7027ba4c
iagogomes96 <i.goomees96@gmail.com>
2023-09-10 20:35:22 -0300

    HTML e CSS do exercício site de receitas

--- projeto/settings.py
+++ projeto/settings.py
@@ -56,7 +56,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
         "DIRS": [
-            BASE_DIR / "recipes" / "templates" / "recipes",
+            BASE_DIR / "recipes" / "templates",
             BASE_DIR / "base_templates",
         ],
         "APP_DIRS": True,
--- recipes/views.py
+++ recipes/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "home.html")
-
-
-def contato(resquest):
-    return HttpResponse("Contato")
-
-
-def sobre(request):
-    return HttpResponse("Sobre")
+    return render(
+        request,
+        "recipes/pages/home.html",
+        context={
+            "name": "Iago Gomes",
+        },
+    )
